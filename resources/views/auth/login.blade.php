1b47a5fe2c161b5953a4062debaaa7c1b9588100
unknown <nunezgeorge05@gmail.com>
2024-06-20 17:48:38 +0800

    login picture

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,6 @@
 
     <title>Login</title>
 
-
 </head>
 
 <body>
@@ -16,12 +15,10 @@
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
-        <div class="bglogin bg-[url('/public/img/figg-bg.png')] w-full min-h-screen bg-cover flex justify-center">
-            <div class="p-4 ml-72 mt-32">
+        <div class="bglogin bg-[url('/public/img/login-bg-final.png')] w-full min-h-screen bg-cover flex justify-start">
+            <div class="p-4 ml-14 mt-32">
                 <div class="card bg-white h-96 w-96 rounded-md shadow-md">
-                    <div class= "bg-[url('/public/img/tesd-eo-logo.png')] bg-gray-200 w-full bg-cover bg-center border-2 h-20 p-4 flex justify-center items-center rounded-t-md" style="padding-top: 50px">
-                       
-
+                    <div class="bg-[url('/public/img/tesd-eo-logo.png')] bg-gray-200 w-full bg-cover bg-center border-2 h-20 p-4 flex justify-center items-center rounded-t-md" style="padding-top: 50px">
                     </div>
 
                     <div class="flex flex-col items-center justify-center p-4 space-y-4 text-sm mt-4">
@@ -29,8 +26,8 @@
                         <div class="w-64">
                             <label for="email">Email</label>
                             <input type="email" id="email" name="email"
-                                class="p-1 border-2 border-black rounded-md w-full"  required>
-                                @error('email')
+                                class="p-1 border-2 border-black rounded-md w-full" required>
+                            @error('email')
                                 <div>{{ $message }}</div>
                             @enderror
                         </div>
@@ -38,19 +35,14 @@
                             <label for="password">Password</label>
                             <input type="password" id="password" name="password"
                                 class="p-1 border-2 border-black rounded-md w-full" required>
-                                @error('password')
+                            @error('password')
                                 <div>{{ $message }}</div>
                             @enderror
                         </div>
                         <button type="submit" class="bg-blue-700 hover:bg-blue-600 px-6 py-2 text-white rounded-md mt-4">Login</button>
                     </div>
                 </div>
-
             </div>
-
-
-        </div>
-
         </div>
     </form>
 
